f3c211177d60ea7b1e7e47a372d9af0276da5b8c
trioxtron <trioxgames@gmail.com>
2024-01-13 12:19:22 +0100

    feat: set counter any given value

--- api/api.go
+++ api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -14,11 +15,24 @@ func (cn *Counter) GetValue(c *fiber.Ctx) error {
 	return c.SendString(fmt.Sprint(cn.value))
 }
 
+func (cn *Counter) SetValue(c *fiber.Ctx) error {
+	value, err := strconv.Atoi(c.FormValue("value"))
+	if err != nil {
+		return c.SendString("value must be a number")
+	}
+	cn.value = value
+	return c.SendString(fmt.Sprint(cn.value))
+}
+
+func (c *Counter) ReturnValue() int {
+	return c.value
+}
+
 func (c *Counter) Increase() int {
 	c.value++
-    return c.value
+	return c.value
 }
 func (c *Counter) Decrease() int {
 	c.value--
-    return c.value
+	return c.value
 }
--- main.go
+++ main.go
@@ -3,32 +3,45 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/template/html/v2"
 	"github.com/trioxtron/golang-service/api"
 )
 
 func setupRoutes(app *fiber.App) {
-    counter := &api.Counter{}
-    app.Static("/", "./src/index.html")
-    app.Static("/src/tailwind.css", "./src/tailwind.css")
-    app.Get("/countervalue", func(c *fiber.Ctx) error {
-        return counter.GetValue(c)
-    })
-    app.Post("/increase", func(c *fiber.Ctx) error {
-        counter.Increase()
-        return counter.GetValue(c) 
-    })
-    app.Post("/decrease", func(c *fiber.Ctx) error {
-        counter.Decrease()
-        return counter.GetValue(c) 
-    })
+	counter := &api.Counter{}
+	app.Static("/src/tailwind.css", "./src/tailwind.css")
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Counter": counter.ReturnValue(),
+		})
+	})
+
+	app.Get("/countervalue", func(c *fiber.Ctx) error {
+		return counter.GetValue(c)
+	})
+
+	app.Post("/setvalue", func(c *fiber.Ctx) error {
+		return counter.SetValue(c)
+	})
+	app.Post("/increase", func(c *fiber.Ctx) error {
+		counter.Increase()
+		return counter.GetValue(c)
+	})
+	app.Post("/decrease", func(c *fiber.Ctx) error {
+		counter.Decrease()
+		return counter.GetValue(c)
+	})
 }
 
 func main() {
-	app := fiber.New()
-    app.Use(cors.New(cors.Config{
-        AllowOrigins: "*",
-    }))
+	engine := html.New("./views", ".html")
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+	}))
 	setupRoutes(app)
 	app.Listen(":3000")
-
 }
